333f7d44aa15c0f461550e781533da9250c2812f
baregamyan <baregamyan@gmail.com>
2021-11-14 22:28:53 +0300

    finish 'Homework 3'

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
+import { getRandomInt } from './utils/common';
+
 /**
  * Default player hp value/
  */
 const HP_DEFAULT_VALUE = 100;
 
+const DamageValue = {
+  MIN: 1,
+  MAX: 20,
+};
+
 /**
  * Container where players appear.
  */
 const $arenas = document.querySelector('.arenas');
+
+const $randomButton = document.querySelector('.button');
 
 const player1 = {
   player: 1,
@@ -74,6 +83,79 @@ function createPlayer(playerObj) {
 
   return $player;
 }
+
+/**
+ * Create win title.
+ * @param {Object} player - Player's param.
+ * @return {HTMLElement}
+ */
+function playerWin(player) {
+  const $loseTitle = createElement('div', 'winTitle');
+  $loseTitle.innerText = `${player.name} wins`;
+
+  return $loseTitle;
+}
+
+/**
+ * Create draw title.
+ * @return {HTMLElement}
+ */
+function playersDraw() {
+  const $drawTitle = createElement('div', 'drawTitle');
+  $drawTitle.innerHTML = 'Ha, draw! You are all losers!';
+
+  return $drawTitle;
+}
+
+/**
+ * Change player's hp.
+ * @param {Object} player - Player's param.
+ */
+function changeHp(player) {
+  const playerClass = `player${player.player}`;
+  const $playerLive = document.querySelector(`.${playerClass} .life`);
+  // eslint-disable-next-line no-param-reassign
+  player.hp -= getRandomInt(DamageValue.MIN, DamageValue.MAX);
+
+  if (player.hp < 0) {
+  // eslint-disable-next-line no-param-reassign
+    player.hp = 0;
+  }
+
+  $playerLive.style.width = `${player.hp}%`;
+}
+
+/**
+ * Render result title.
+ * @param {HTMLElement} result - Result title.
+ */
+function showResult(result) {
+  if (!result) {
+    return;
+  }
+
+  $randomButton.setAttribute('disabled', true);
+  $arenas.appendChild(result);
+}
+
+// eslint-disable-next-line consistent-return
+$randomButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  changeHp(player1);
+  changeHp(player2);
+
+  if ((player1.hp === 0) && (player2.hp === 0)) {
+    return showResult(playersDraw());
+  }
+
+  if (player1.hp === 0) {
+    return showResult(playerWin(player2));
+  }
+
+  if (player2.hp === 0) {
+    return showResult(playerWin(player1));
+  }
+});
 
 $arenas.appendChild(createPlayer(player1));
 $arenas.appendChild(createPlayer(player2));
