8480baadf721023579e16da0cbe609c49ab80c7c
baregamyan <baregamyan@gmail.com>
2021-11-14 20:36:52 +0300

    add consts from the mocks to separate directory

---
+++ src/const.js
@@ -0,0 +1,36 @@
+/**
+ * Some names of Mortal Kombat characters.
+ */
+const NAMES = [
+  'Scorpion',
+  'Kitana',
+  'Liukang',
+  'Sonya',
+  'subzero',
+];
+
+/**
+ * Some names weapons that characters use.
+ */
+const WEAPONS = [
+  'Katana',
+  'Ice',
+  'Fireball',
+  'Strom',
+  'Titan Hands',
+  'Smart Brain',
+];
+
+/**
+ * Min and max player HP value.
+ */
+const Hp = {
+  MIN: 0,
+  MAX: 100,
+};
+
+export {
+  NAMES,
+  WEAPONS,
+  Hp,
+};
